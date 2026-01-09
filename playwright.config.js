9081fe8a8edced6d0c22f5631b3ed0e8dd98d151
Vineet Rao <vineethere13@gmail.com>
2026-01-09 01:28:08 -0500

    send playwright JUnit results to Datadog fix junit error

--- playwright.config.js
+++ playwright.config.js
@@ -23,7 +23,10 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: [
+    ['html'],
+    ['junit', { outputFile: 'test-results/junit.xml' }],
+  ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
@@ -78,10 +81,6 @@ export default defineConfig({
   //   reuseExistingServer: !process.env.CI,
   // },
 
-  reporter: [
-  ['html'],
-  ['junit', { outputFile: 'test-results/junit.xml' }]
-],
 
 });
 
